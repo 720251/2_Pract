05f9d7c89bd0a8978ae9888d9305d38e762831a4
Alba <720251@unizar.es>
2021-11-01 15:49:58 +0100

    Create Practica3.5.py
    Se convierte la lectura del COM en una lista, se separa para crear un diccionario y se realiza un append cada 5 segundos (llega el dato cada segundo), cuando cumple los 5 segundos, calcula media y desviación estandar de cada variable y resetea el contador para volver a empezar ¿SE DEBE HACER CADA 5 SEG MEAN Y STD O TODO EL RATO Y CADA 5 SEG RESETEAR CONTADOR?? FALTA PLOT

---
+++ P3/Practica3.5.py
@@ -0,0 +1,49 @@
+import serial
+import numpy as np
+
+PuertoSerie = serial.Serial('COM4', baudrate=115200, timeout=1.0)
+signal = {'X' : [], 'Y' : [], 'Z' : [], 'A' : []}
+contador = 0
+
+while True:
+
+    try:
+        esp32 = PuertoSerie.readline()
+        datosCaracter = ""
+        for valor in esp32:
+            datosCaracter = datosCaracter + chr(valor)
+        prueba = datosCaracter.replace(" ", ";").replace("\n","").replace("\r","").split(";")
+        listaTexto =list(prueba)
+        #print(listaTexto)
+        if contador < 5:
+            signal['X'].append(float(listaTexto[1]))
+            signal['Y'].append(float(listaTexto[3]))
+            signal['Z'].append(float(listaTexto[5]))
+            signal['A'].append(float(listaTexto[7]))
+            contador = contador + 1
+            print(contador)
+            if contador == 5:
+                meanX = np.mean(signal['X'])
+                stdX = np.std(signal['X'])
+                meanY = np.mean(signal['Y'])
+                stdY = np.std(signal['Y'])
+                meanZ = np.mean(signal['Z'])
+                stdZ = np.std(signal['Z'])
+                meanA = np.mean(signal['A'])
+                stdA = np.std(signal['A'])
+        else:
+            contador = 0;
+            signal['X'].clear()
+            signal['Y'].clear()
+            signal['Z'].clear()
+            signal['A'].clear()
+        print(signal)
+
+        
+    except KeyboardInterrupt:
+        break
+    
+PuertoSerie.close()
+
+
+
