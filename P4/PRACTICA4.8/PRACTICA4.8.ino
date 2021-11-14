a087e39333b7f16390d2cd81d257317bda3a73b6
Alba <720251@unizar.es>
2021-11-14 21:31:50 +0100

    Update PRACTICA4.8.ino

--- P4/PRACTICA4.8/PRACTICA4.8.ino
+++ P4/PRACTICA4.8/PRACTICA4.8.ino
@@ -1,11 +1,11 @@
 #include "config.h"
 
-int contador = 0;
+int contar = 0;
 
 #define IO_LOOP_DELAY 5000
 unsigned long actualizacion = 0;
 
-AdafruitIO_Feed *contador = io.feed("contador");
+AdafruitIO_Feed *prueba = io.feed("prueba");
 
 void setup() {
   Serial.begin(115200);
@@ -13,7 +13,7 @@ void setup() {
   Serial.print("Connecting to Adafruit IO");
   io.connect();
 
-  contador->onMessage(Mensaje);
+  prueba->onMessage(Mensaje);
 
   while(io.status() < AIO_CONNECTED) {
     Serial.print(".");
@@ -22,7 +22,7 @@ void setup() {
 
   Serial.println();
   Serial.println(io.statusText());
-  contador->get();
+  prueba->get();
 }
 
 void loop() {
@@ -30,10 +30,10 @@ void loop() {
 
   if (millis() > (actualizacion + IO_LOOP_DELAY)) {
     Serial.print("Enviado -> ");
-    Serial.println(contador);
-    contador->save(contador);
+    Serial.println(contar);
+    prueba->save(contar);
 
-    contador++;
+    contar++;
 
     actualizacion = millis();
   }
