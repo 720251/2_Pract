5abed756d06cdc2b6867237efa480e88ea77ce3e
Alba <93447980+720251@users.noreply.github.com>
2021-11-20 18:07:42 +0100

    Create PRACTICA5.3.ino

---
+++ P5/PRACTICA5.3/PRACTICA5.3.ino
@@ -0,0 +1,62 @@
+//Ejemplo obtenido de la placa DOIT ESP32
+
+#include "sys/time.h"
+#include "BLEDevice.h"
+#include "BLEUtils.h"
+#include "BLEServer.h"
+#include "BLEBeacon.h"
+#include "esp_sleep.h"
+
+#define GPIO_DEEP_SLEEP_DURATION     10  // sleep x seconds and then wake up
+RTC_DATA_ATTR static time_t last;        // remember last boot in RTC Memory
+RTC_DATA_ATTR static uint32_t bootcount; // remember number of boots in RTC Memory
+
+
+BLEAdvertising *pAdvertising;   // BLE Advertisement type
+struct timeval now;
+
+#define BEACON_UUID "8b21e714-4a01-11ec-81d3-0242ac720251" // UUID 1 128-Bit (may use linux tool uuidgen or random numbers via https://www.uuidgenerator.net/)
+
+void setBeacon() {
+
+  BLEBeacon oBeacon = BLEBeacon();
+  oBeacon.setManufacturerId(0x4C00); // fake Apple 0x004C LSB (ENDIAN_CHANGE_U16!)
+  oBeacon.setProximityUUID(BLEUUID(BEACON_UUID));
+  oBeacon.setMajor((bootcount & 0xFFFF0000) >> 16);
+  oBeacon.setMinor(bootcount & 0xFFFF);
+  BLEAdvertisementData oAdvertisementData = BLEAdvertisementData();
+  BLEAdvertisementData oScanResponseData = BLEAdvertisementData();
+
+  oAdvertisementData.setFlags(0x04); // BR_EDR_NOT_SUPPORTED 0x04
+
+  std::string strServiceData = "";
+
+  strServiceData += (char)26;     // Len
+  strServiceData += (char)0xFF;   // Type
+  strServiceData += oBeacon.getData();
+  oAdvertisementData.addData(strServiceData);
+
+  pAdvertising->setAdvertisementData(oAdvertisementData);
+  pAdvertising->setScanResponseData(oScanResponseData);
+}
+
+void setup() {
+
+  Serial.begin(115200);
+  Serial.printf("start ESP32 %d\n", bootcount++);
+
+  // Create the BLE Device
+  BLEDevice::init("720251");
+  // Create the BLE Server
+  BLEServer *pServer = BLEDevice::createServer(); // <-- no longer required to instantiate BLEServer, less flash and ram usage
+  pAdvertising = BLEDevice::getAdvertising();
+  BLEDevice::startAdvertising();
+  setBeacon();
+  // Start advertising
+  pAdvertising->start();
+  Serial.println("Advertizing started...");
+  delay(100);
+}
+
+void loop() {
+}
